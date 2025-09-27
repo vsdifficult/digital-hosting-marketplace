8149750e416d49f42cf60dda05ccf2305520097c
KateLy30 <kateshytova2006@gmail.com>
2025-09-27 13:11:43 +0300

    features: add DTO Server and User

---
+++ src/HostMarket.Infrastructure/Data/DTO/ServerDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostMarket.Infrastructure.Data.DTO
+{
+    public class ServerDTO
+    {
+        public Guid ownerId { get; set; }
+        public string ServerName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public ServerStatus ServStatus { get; set; }
+    }
+    
+}
---
+++ src/HostMarket.Infrastructure/Data/DTO/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostMarket.Infrastructure.Data.DTO
+{
+    public class UserDTO
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public decimal Balance { get; set; }
+        public bool IsVerify { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
