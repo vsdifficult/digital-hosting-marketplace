5fbdd7a38b9c633ad357fd20dd0485714043fbc2
KateLy30 <kateshytova2006@gmail.com>
2025-10-04 10:16:32 +0300

    Created UserRepository/IUserRepository/IRepository

---
+++ src/HostMarket.Infrastructure/Data/EntityFramework/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using HostMarket.Infrastructure.Data.Entities;
+using HostMarket.Infrastructure.Data.EntityFramework.Configurations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostMarket.Infrastructure.Data.EntityFramework
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; } = null!;
+        public DbSet<ServerEntity> Servers { get; set; } = null!;
+        public DbSet<TransactionEntity> Transactions { get; set; } = null!;
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.HasDefaultSchema("DatabaseSchema");
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ServerConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+        }
+    }
+}
