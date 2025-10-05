f013ea221af13a93d3a4f06a483d0a1411091ebf
ilya li <101355829+vsdifficult@users.noreply.github.com>
2025-10-05 09:11:27 +0000

    Update

---
+++ src/HostMarket.Core/Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostMarket.Core.Repositories;
+
+public interface IRepository<T, TKey> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(TKey id);
+    Task<TKey> CreateAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(TKey id);
+}
