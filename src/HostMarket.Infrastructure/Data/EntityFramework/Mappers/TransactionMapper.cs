b76b741f7c57987d31281691eb8b22f3f66c737b
KateLy30 <kateshytova2006@gmail.com>
2025-10-26 16:10:41 +0300

    feature/TransactionRepository

---
+++ src/HostMarket.Infrastructure/Data/EntityFramework/Mappers/TransactionMapper.cs
@@ -0,0 +1,52 @@
+﻿using HostMarket.Infrastructure.Data.Entities;
+using HostMarket.Shared.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostMarket.Infrastructure.Data.EntityFramework.Mappers
+{
+    public class TransactionMapper
+    {
+        public static TransactionDto FromEntityToDto(TransactionEntity entity)
+        {
+            return new TransactionDto
+            {
+                Id = entity.Id,
+                userId = entity.userId,
+                serverId = entity.serverId,
+                Amount = entity.Amount,
+                transactionDate = entity.transactionDate,
+                CreateAt = entity.CreateAt,
+                UpdateAt = entity.UpdateAt,
+                Status = entity.Status
+            };
+        }
+        public static TransactionEntity FromDtoToEnity(TransactionDto dto)
+        {
+            return new TransactionEntity
+            {
+                Id = dto.Id,
+                userId = dto.userId,
+                serverId = dto.serverId,
+                Amount = dto.Amount,
+                transactionDate = dto.transactionDate,
+                CreateAt = dto.CreateAt,
+                UpdateAt = dto.UpdateAt,
+                Status = dto.Status
+
+            };
+        }
+        public static void Update(TransactionEntity entity, TransactionDto dto)
+        {
+            entity.userId = dto.userId;
+            entity.serverId = dto.serverId;
+            entity.Amount = dto.Amount;
+            entity.transactionDate = dto.transactionDate;
+            entity.UpdateAt = DateTime.UtcNow;
+            entity.Status = dto.Status;
+        }
+    }
+}
---
+++ src/HostMarket.Infrastructure/Data/EntityFramework/Repositories/TransactionRepository.cs
@@ -0,0 +1,66 @@
+﻿using HostMarket.Core.Repositories;
+using HostMarket.Infrastructure.Data.EntityFramework.Mappers;
+using HostMarket.Shared.Dto;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HostMarket.Infrastructure.Data.EntityFramework.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly DataContext _context;
+        public TransactionRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task<Guid> CreateAsync(TransactionDto entity)
+        {
+            var transactionEntity = TransactionMapper.FromDtoToEnity(entity);
+            transactionEntity.Id = Guid.NewGuid();
+            transactionEntity.CreateAt = DateTime.UtcNow;
+            transactionEntity.transactionDate = DateTime.UtcNow;
+
+            await _context.Transactions.AddAsync(transactionEntity);
+            await _context.SaveChangesAsync();
+
+            return transactionEntity.Id;
+        }
+
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null) return false;
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<IEnumerable<TransactionDto>> GetAllAsync()
+        {
+            return await _context.Transactions.Select(t => TransactionMapper.FromEntityToDto(t)).ToListAsync();
+        }
+
+        public async Task<TransactionDto?> GetByIdAsync(Guid id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+            if (transaction == null) return null;
+            return TransactionMapper.FromEntityToDto(transaction);
+        }
+
+
+        public async Task<bool> UpdateAsync(TransactionDto entity)
+        {
+            var transaction = await _context.Transactions.FindAsync(entity.Id);
+            if (transaction == null) return false;
+            TransactionMapper.Update(transaction, entity);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
