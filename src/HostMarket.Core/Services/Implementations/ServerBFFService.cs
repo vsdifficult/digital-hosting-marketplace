f8874aac5060840c530f5cd19fa9993ae0bbcc1d
KilluaZoludyck <144722702+KilluaZoludyck@users.noreply.github.com>
2025-11-25 19:42:14 +0300

    feature/ I+ServerBFFService

---
+++ src/HostMarket.Core/Services/Implementations/ServerBFFService.cs
@@ -0,0 +1,12 @@
+using HostMarket.Core.Services.Interfaces;
+using HostMarket.Shared.Dto;
+using HostMarket.Infrastructure.Data;
+using HostMarket.Infrastructure.Data.DTO;
+using HostMarket.Shared.Models;
+using HostMarket.Core.Repositories;
+using System.Data.Common;
+using System.Net.NetworkInformation;
+
+namespace HostMarket.Core.Services.Implementations.Bff;
+
+
---
+++ src/HostMarket.Core/Services/Interfaces/IServerBFFService.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HostMarket.Infrastructure.Data.DTO;
+using HostMarket.Shared.Dto;
+using HostMarket.Shared.Models;
+
+namespace HostMarket.Core.Services.Interfaces;
+
